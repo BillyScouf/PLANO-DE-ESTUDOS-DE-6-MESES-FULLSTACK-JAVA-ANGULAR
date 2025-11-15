4f42bdc95c702db91d1b6f7430e20458e021ec37
Andersio Lopes <andersiolopes2804@gmail.com>
2025-11-15 19:43:49 +0100

    Programa para encontrar o maior e menor numero do array

--- src/com/planodeestudos/fundamentos/maiorEMenor.java
+++ src/com/planodeestudos/fundamentos/maiorEMenor.java
@@ -1,5 +1,31 @@
 package com.planodeestudos.fundamentos;
 
+import java.util.Scanner;
+
 public class maiorEMenor {
 
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+// Array de números
+        double numeros[] = {65.5,23,87,4,3,0,-10,34,-5};
+
+        double maior = numeros[0];
+        double menor = numeros[0];
+
+        // Laço para encontrar maior e menor número
+
+        for(int i = 0; i < numeros.length; i++){
+
+            // Comparações para encontrar maior e menor número
+            if(numeros[i] > maior){
+                maior = numeros[i];
+            }
+            if(numeros[i] < menor){
+                menor = numeros[i];
+            }
+        }
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menor número é: " + menor);
+    }
 }
